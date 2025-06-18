c49cf9959e7c236b4e6e6ee39a1967201be6e08e
Angel Rivas <angel.rivas-gonzalez@mmmhc.com>
2025-06-18 08:24:47 -0600

    Initial Commit

---
+++ NYTArticles/NYTArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTArticlesApp.swift
+//  NYTArticles
+//
+//  Created by Angel Rivas on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NYTArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
